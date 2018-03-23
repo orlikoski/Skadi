6567a743a770af16cc9f8528ec9bdd12493e71ea
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 23:53:15 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -14,13 +14,13 @@ def run():
     print(count)
 
     if count == 2:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1]))
     elif count == 3:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2]))
     elif count == 4:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3],arg1=sys.argv[4]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3]))
     elif count == 5:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3],arg1=sys.argv[4],arg2=sys.argv[5]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
     else:
         print("WARNING!! Invalid number of arguments. Exiting")
 
