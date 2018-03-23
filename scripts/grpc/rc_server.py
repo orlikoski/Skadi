0e911eaad84c6657d3c19137b0def22d6219e333
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 22:04:31 -0500

    script update

--- scripts/grpc/rc_server.py
+++ scripts/grpc/rc_server.py
@@ -22,6 +22,8 @@ class RC(rc_pb2_grpc.RCServicer):
         args = ' '.join(request.arg)
         #command = "python3 " + rcpy + " " + request.service + " --" + request.flag + " " + args
         #command = "python3 " + rcpy + " " + request.service
+        print(requests.service)
+        exit(1)
         command = ["/usr/bin/python3",rcpy,request.service]
         #cmd = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         cmd = subprocess.Popen(command)
