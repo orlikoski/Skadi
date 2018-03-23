5323225750b0e6eb66adea4aa924f2d704673e1e
@AlanOrlikoski <rough007@users.noreply.github.com>
2018-03-23 09:15:46 -0500

    Grpc Updates (#34)
    Updated the GRPC process to use port 10101 and provide input validation to prevent injection attacks

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -1,20 +1,32 @@
+#!/usr/bin/python
 """The Python implementation of the GRPC rc.RC client."""
-
 from __future__ import print_function
-
-import grpc
-
-import rc_pb2
-import rc_pb2_grpc
+import grpc, rc_pb2, rc_pb2_grpc, sys
 
 
 def run():                         
-    channel = grpc.insecure_channel('localhost:50051')
+    channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
-    
-    response = stub.ExecuteRC(rc_pb2.RCRequest(service='dp', flag='mv_local',arg=["$(echo a| base64)", "$(echo a| base64)"]))
-    print("RC client received: " + response.message)
-    
-                                                                      
+
+    unapproved_chars = set(';&|')
+    strtest = ','.join(sys.argv)
+    if any((char in unapproved_chars) for char in strtest):
+        print("ERROR!! Unapproved chars in string. Exiting")
+        exit(1)
+
+    count = len(sys.argv)
+
+    if count == 2:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1]))
+    elif count == 3:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2]))
+    elif count == 4:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3]))
+    elif count == 5:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
+    else:
+        print("WARNING!! Invalid number of arguments. Exiting")
+
 if __name__ == '__main__':
     run()
+
