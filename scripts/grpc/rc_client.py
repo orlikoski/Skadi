5fcf023daeeb17c0d7b07ae1ebed9eadff187671
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 20:32:56 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 """The Python implementation of the GRPC rc.RC client."""
 from __future__ import print_function
-
-import grpc
-
-import rc_pb2
-import rc_pb2_grpc
+import grpc, rc_pb2, rc_pb2_grpc
 
 
 def run():                         
+    # Collect arguments
+    service_arg = sys.argv[1]
+    flag_arg = sys.argv[2]
+    arg_arg = sys.argv[3] + " " + sys.argv[4]
+
     channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
-    
-    response = stub.ExecuteRC(rc_pb2.RCRequest(service='dp', flag='mv_local',arg=["$(echo a| base64)", "$(echo a| base64)"]))
+    response = stub.ExecuteRC(rc_pb2.RCRequest(service=service_arg, flag=flag_arg,arg=arg_arg))
     print("RC client received: " + response.message)
-    
-                                                                      
+
 if __name__ == '__main__':
     run()
+
