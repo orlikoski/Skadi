8c1dc67fc6ea505823985035802e005a87a70ab3
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 20:37:21 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 """The Python implementation of the GRPC rc.RC client."""
 from __future__ import print_function
-import grpc, rc_pb2, rc_pb2_grpc
+import grpc, rc_pb2, rc_pb2_grpc, sys
 
 
 def run():                         
     # Collect arguments
-    service_arg = sys.argv[1]
-    flag_arg = sys.argv[2]
-    arg_arg = sys.argv[3] + " " + sys.argv[4]
+    try:
+        service_arg = sys.argv[1]
+        flag_arg = sys.argv[2]
+        arg_arg = sys.argv[3] + " " + sys.argv[4]
+    except:
+        print(sys.argv)
+        print("ERROR: Arguements not parsed correctly")
+
 
     channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
