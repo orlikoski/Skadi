521b966967d9d597fbd518bc488da352a86c5955
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 23:10:19 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -9,10 +9,16 @@ def run():
     stub = rc_pb2_grpc.RCStub(channel)
     #response = stub.ExecuteRC(rc_pb2.RCRequest(service=service_arg, flag=flag_arg,arg=arg_arg))
 
-    if sys.argv[4]:
+    if sys.argv[1]:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1]))
+    elif sys.argv[2]:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2]))
+    elif sys.argv[3]:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3]))
+    elif sys.argv[4]:
         response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
     else:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
+        print("WARNING!! Invalid number of arguments. Exiting")
 
 if __name__ == '__main__':
     run()
