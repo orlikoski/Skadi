3111982718126652c81cf3fa3c032dc3abf53fb0
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 22:27:07 -0500

    script update

--- scripts/grpc/rc_server.py
+++ scripts/grpc/rc_server.py
@@ -20,14 +20,15 @@ class RC(rc_pb2_grpc.RCServicer):
 
     def ExecuteRC(self, request, context):
         args = ' '.join(request.arg)
-        #command = "python3 " + rcpy + " " + request.service + " --" + request.flag + " " + args
-        #command = "python3 " + rcpy + " " + request.service
-        input_args = request.service.split(' ')
+        #input_args = request.service.split(' ')
+        input_args = [request.service, request.flag, request.arg1]
+        if request.arg2:
+            input_args.append(request.arg2)
         command = ["/usr/bin/python3",rcpy]
-        for item in input_args:
-            command.append(item)
-        #cmd = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        cmd = subprocess.Popen(command)
+        # for item in input_args:
+        #     command.append(item)
+
+        cmd = subprocess.Popen(command,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         status = cmd.wait()
         return rc_pb2.RCReply(message="ERROR: Command failed to execute")
 
