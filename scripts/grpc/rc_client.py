5f812bd9dd16f39354bea39205cb2ae49db524ed
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 22:56:00 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -7,6 +7,7 @@ import grpc, rc_pb2, rc_pb2_grpc, sys
 def run():                         
     channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
+    #response = stub.ExecuteRC(rc_pb2.RCRequest(service=service_arg, flag=flag_arg,arg=arg_arg))
 
     if sys.argv[4]:
         response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
