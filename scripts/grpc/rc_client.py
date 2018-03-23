31adb3063ac488d624ba6144b3a917d7603145ec
Alan Orlikoski <alan@orlikoski.com>
2018-03-22 20:56:11 -0500

    script update

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -5,19 +5,10 @@ import grpc, rc_pb2, rc_pb2_grpc, sys
 
 
 def run():                         
-    # Collect arguments
-    try:
-        service_arg = sys.argv[1]
-        flag_arg = sys.argv[2]
-        arg_arg = sys.argv[3] + " " + sys.argv[4]
-    except:
-        print(sys.argv)
-        print("ERROR: Arguements not parsed correctly")
-
-
     channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
-    response = stub.ExecuteRC(rc_pb2.RCRequest(service=service_arg, flag=flag_arg,arg=arg_arg))
+    #response = stub.ExecuteRC(rc_pb2.RCRequest(service=service_arg, flag=flag_arg,arg=arg_arg))
+    response = stub.ExecuteRC(rc_pb2.RCRequest(service=' '.join(sys.argv)))
     print("RC client received: " + response.message)
 
 if __name__ == '__main__':
