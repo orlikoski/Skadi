e632a34990a664fddf10e6b8294aadd611ead6c9
Alan Orlikoski <rough007@users.noreply.github.com>
2018-03-25 13:55:28 -0500

    updating scripts

--- scripts/automation.sh
+++ scripts/automation.sh
@@ -2,6 +2,7 @@
 
 echo "Installing pip3 for Python3 if not installed"
 sudo apt install python3-pip
+sudo -H pip3 install --upgrade pip
 echo "Getting Python dependencies"
 sudo -H pip3 install -r requirements.txt
 
