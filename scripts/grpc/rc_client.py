a00c0e86fef6f9c19125ffab95f8dd0bfc408494
Alan Orlikoski <orlikoski@users.noreply.github.com>
2018-04-10 18:28:53 -0500

    Fully implementing logging. Commented out cloud dp functions. Commented out OS commands. Fixed cdqr function. Increased RCE invlaid character set. Completed gRPC automation functions

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -4,10 +4,27 @@ from __future__ import print_function
 import grpc, rc_pb2, rc_pb2_grpc, sys, argparse
 
 
+def parse_input(commands):
+    count = len(commands)
+    if count == 1:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=commands[0]))
+    elif count == 2:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=commands[0],flag=commands[1]))
+    elif count == 3:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=commands[0],flag=commands[1],arg1=commands[2]))
+    elif count == 4:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=commands[0],flag=commands[1],arg1=commands[2],arg2=commands[3]))
+    else:
+        print("WARNING!! Invalid number of arguments. Exiting")
+        exit(1)
+    return response
+
+
 def main():
     version = "Skadi Automation Engine Version: 1.0.0"
-    parser = argparse.ArgumentParser(description='Skadi Automation Engine')
-    parser.add_argument('-s','--server',nargs=1,help='Routable Domain Name or IP address of Skadi server')
+    parser = argparse.ArgumentParser(description=version)
+    parser.add_argument('-s','--server',nargs=1,required=True,help='Routable Domain Name or IP address of Skadi server')
+    parser.add_argument('commands', type=str, nargs='+')
     parser.add_argument('-v','--version', action='version', version=version)
     args=parser.parse_args()
 
@@ -17,23 +34,18 @@ def main():
         print("ERROR!! Unapproved chars in string. Exiting")
         exit(1)
 
-    count = len(sys.argv)
+    
     channel = grpc.insecure_channel('localhost:10101')
     stub = rc_pb2_grpc.RCStub(channel)
 
+    response = ""
+    if args.commands:
+        response = parse_input(args.commands)
+    else:
+        print("WARNING: No commands provided. Exiting")
 
     print("Attempting to send command to server\n")
-    response = ""
-    if count == 2:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1]))
-    elif count == 3:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2]))
-    elif count == 4:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3]))
-    elif count == 5:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
-    else:
-        print("WARNING!! Invalid number of arguments. Exiting")
+
 
     print(response.message)
 
