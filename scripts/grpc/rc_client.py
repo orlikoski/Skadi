5baa4b2e040f12daf3c953a0b8216148fe56675c
@AlanOrlikoski <9388572+orlikoski@users.noreply.github.com>
2018-04-11 10:24:20 -0500

    Automation Pack
    Fully implementing logging. Commented out cloud dp functions. Commented out OS commands. Fixed cdqr function. Increased RCE invlaid character set. Completed gRPC automation functions
    
    Added Automation support for:
    TimeSketch
      - Add user / change password on user
      - Delete timelines
    ElasticSearch
      - List indices and their health
      - Delete index
    Moving Data
      - Move data on local file system
    Processing data
      - Process artifacts with CDQR (all functions of CDQR available)
    
    Two automation methods supported:
      - Both methods use dedicated account `ottomate`
      - SSH method
      - gRPC client / server method

--- scripts/grpc/rc_client.py
+++ scripts/grpc/rc_client.py
@@ -3,35 +3,45 @@
 from __future__ import print_function
 import grpc, rc_pb2, rc_pb2_grpc, sys
 
-
-def run():                         
-    channel = grpc.insecure_channel('localhost:10101')
-    stub = rc_pb2_grpc.RCStub(channel)
-
-    unapproved_chars = set(';&|')
+def main():
+    usage = "rc_client.py <server IP address or routable domain name> <commands to send to rc_server.py>"
+    version = "Skadi Automation Client Version: 1.0.0"
+    print("Starting "+version)
+    unapproved_chars = set('`~!#$&*()\t{[|\\;\'\"<>?')
     strtest = ','.join(sys.argv)
     if any((char in unapproved_chars) for char in strtest):
+        print(usage)
         print("ERROR!! Unapproved chars in string. Exiting")
         exit(1)
 
     count = len(sys.argv)
+    if not sys.argv[1]:
+        print(usage)
+        print("WARNING: No server information found. Please provide a routable domain name or IP address. Exiting")
+        exit(1)
+
+    server = sys.argv[1]
+    channel = grpc.insecure_channel(server+':10101')
+    stub = rc_pb2_grpc.RCStub(channel)
 
     print("Attempting to send command to server\n")
     response = ""
-    if count == 2:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1]))
-    elif count == 3:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2]))
+    if count == 3:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2]))
     elif count == 4:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3]))
     elif count == 5:
-        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[1],flag=sys.argv[2],arg1=sys.argv[3],arg2=sys.argv[4]))
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3],arg1=sys.argv[4]))
+    elif count == 6:
+        response = stub.ExecuteRC(rc_pb2.RCRequest(service=sys.argv[2],flag=sys.argv[3],arg1=sys.argv[4],arg2=sys.argv[5]))
     else:
+        print(usage)
         print("WARNING!! Invalid number of arguments. Exiting")
 
     print(response.message)
+    print("Command sent")
+    print("Stopping "+version)
 
-    print("COMPLETE")
 if __name__ == '__main__':
-    run()
+    main()
 
