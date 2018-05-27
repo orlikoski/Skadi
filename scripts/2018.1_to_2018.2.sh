06f1b5bfa1b757ac239d0bd7aca1027e6d4da4fd
Alan Orlikoski <orlikoski@users.noreply.github.com>
2018-05-26 19:27:41 -0500

    Upgrade script is working secure networking script is working

--- scripts/2018.1_to_2018.2.sh
+++ scripts/2018.1_to_2018.2.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+echo "Upgrading Skadi 2018.1 to 2018.2"
+echo "*********** WARNING ***********"
+echo "All data in the ELK will be lost and the data in TimeSketch could be corrupted (due to the loss of Elasticsearch data) due to this upgrade"
+echo "Additionally, the `Post Installation` instructions from https://github.com/orlikoski/Skadi/wiki/Installation:-OpenSSL-Signed-Installation-Guide will need to be followed"
+echo "If this seems like too much work are will create issues, it may be easier to use the newest version of Skadi and skip the upgrade process"
+echo ""
+echo "root or sudo privileges are required for this installation"
+echo "*********** WARNING ***********"
+echo ""
+read -n 1 -r -s -p "Press any key to continue... or CTRL+C to exit (nothing has been installed)"
+echo ""
+echo ""
 set -e
 # This script converts Skadi 2018.1 to 2018.2
 # NOTE: All of the data in the ELK stack will be lost
@@ -43,12 +55,69 @@ sudo /bin/systemctl daemon-reload &&
 sudo /bin/systemctl enable elasticsearch logstash kibana &&
 sudo /bin/systemctl start elasticsearch logstash kibana
 
-
 # Install Networking pack
 echo "Now installing secure networking pack"
-wget -O /tmp/secure_network.sh https://raw.githubusercontent.com/orlikoski/Skadi/master/scripts/secure_network.sh
-bash /tmp/secure_network.sh
+# Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+sudo sysctl -p
 
+# Install and configure gunicorn
+sudo pip2 install gunicorn
+sudo systemctl stop timesketch
+timesketch_service="W1VuaXRdCkRlc2NyaXB0aW9uPVRpbWVTa2V0Y2ggU2VydmljZQpBZnRlcj1uZXR3b3JrLnRhcmdldAoKW1NlcnZpY2VdClVzZXI9dGltZXNrZXRjaApHcm91cD10aW1lc2tldGNoCkV4ZWNTdGFydD0vdXNyL2xvY2FsL2Jpbi9ndW5pY29ybiAtLXdvcmtlcnMgNCAtLWJpbmQgMTI3LjAuMC4xOjUwMDAgdGltZXNrZXRjaC53c2dpCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK"
+echo $timesketch_service |base64 -d | sudo tee /etc/systemd/system/timesketch.service
+sudo systemctl daemon-reload
+sudo systemctl restart timesketch.service
+
+# Update Kibana to work with forwarding
+sudo systemctl stop kibana
+sudo sed -i "s@\#server.basePath: \"\"@server.basePath: \"/kibana\"@g" /etc/kibana/kibana.yml
+sudo systemctl start kibana
+
+# Install Nginx and web utils
+sudo apt install nginx apache2-utils -y
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
+sudo ufw --force enable
+
+# Configure Nginx for Kibana, Cerebro, and TimeSketch
+nginx_conf="c2VydmVyIHsKICBsaXN0ZW4gODA7CiAgc2VydmVyX25hbWUgXzsKICBjbGllbnRfbWF4X2JvZHlfc2l6ZSA3NU07CiAgcHJveHlfY29ubmVjdF90aW1lb3V0IDkwMHM7CiAgcHJveHlfcmVhZF90aW1lb3V0IDkwMHM7CiAgcm9vdCAgICAgICAgIC91c3Ivc2hhcmUvbmdpbngvaHRtbDsKICBlcnJvcl9wYWdlIDQwNCAvNDA0Lmh0bWw7CiAgICBsb2NhdGlvbiA9IC80MDQuaHRtbCB7fQogIGVycm9yX3BhZ2UgNTAwIDUwMiA1MDMgNTA0IC81MHguaHRtbDsKICAgIGxvY2F0aW9uID0gLzUweC5odG1sIHt9CgogIGVycm9yX2xvZyAgIC92YXIvbG9nL25naW54L2Vycm9yLmxvZzsKICBhY2Nlc3NfbG9nICAvdmFyL2xvZy9uZ2lueC9hY2Nlc3MubG9nOwoKICBsb2NhdGlvbiAvIHsKICAgIHByb3h5X3Bhc3MgaHR0cDovL2xvY2FsaG9zdDo1MDAwOwogICAgcHJveHlfaHR0cF92ZXJzaW9uIDEuMTsKICAgIHByb3h5X3NldF9oZWFkZXIgVXBncmFkZSAkaHR0cF91cGdyYWRlOwogICAgcHJveHlfc2V0X2hlYWRlciBDb25uZWN0aW9uICd1cGdyYWRlJzsKICAgIHByb3h5X3NldF9oZWFkZXIgSG9zdCAkaG9zdDsKICAgIHByb3h5X2NhY2hlX2J5cGFzcyAkaHR0cF91cGdyYWRlOwogICAgc3ViX2ZpbHRlciAnTG9nb3V0PC9hPicgJ0xvZ291dDwvYT48YnI+Jm5ic3A7Jm5ic3A7Jm5ic3A7PGEgdGFyZ2V0PSJfYmxhbmsiIHN0eWxlPSJjb2xvcjojZmZmOyIgaHJlZj0iL2tpYmFuYS8iPktpYmFuYTwvYT4mbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDs8YSB0YXJnZXQ9Il9ibGFuayIgc3R5bGU9ImNvbG9yOiNmZmY7IiBocmVmPSIvY2VyZWJyby8jL292ZXJ2aWV3P2hvc3Q9aHR0cDolMkYlMkZsb2NhbGhvc3Q6OTIwMCI+Q2VyZWJybzwvYT4nOwogICAgc3ViX2ZpbHRlciAnU2lnbiBpbjwvYnV0dG9uPicgJ1NpZ24gaW48L2J1dHRvbj48YnI+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9raWJhbmEvIj5LaWJhbmE8L2E+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9jZXJlYnJvLyMvb3ZlcnZpZXc/aG9zdD1odHRwOiUyRiUyRmxvY2FsaG9zdDo5MjAwIj5DZXJlYnJvPC9hPic7CiAgICBzdWJfZmlsdGVyX29uY2Ugb2ZmOwogIH0KCiAgbG9jYXRpb24gfiBeL2tpYmFuYSguKikkIHsKICAgIHByb3h5X2h0dHBfdmVyc2lvbiAxLjE7CiAgICBwcm94eV9zZXRfaGVhZGVyIFVwZ3JhZGUgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3NldF9oZWFkZXIgQ29ubmVjdGlvbiAndXBncmFkZSc7CiAgICBwcm94eV9zZXRfaGVhZGVyIEhvc3QgJGhvc3Q7CiAgICBwcm94eV9jYWNoZV9ieXBhc3MgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3Bhc3MgIGh0dHA6Ly9sb2NhbGhvc3Q6NTYwMTsKICAgIHJld3JpdGUgXi9raWJhbmEvKC4qKSQgLyQxIGJyZWFrOwogICAgcmV3cml0ZSBeL2tpYmFuYSQgL2tpYmFuYS87CiAgICBhdXRoX2Jhc2ljICJSZXN0cmljdGVkIENvbnRlbnQiOwogICAgYXV0aF9iYXNpY191c2VyX2ZpbGUgL2V0Yy9uZ2lueC8ua2liYW5hX2F1dGg7CiAgfQoKICBsb2NhdGlvbiAvY2VyZWJyby8gewogICAgcHJveHlfcGFzcyBodHRwOi8vbG9jYWxob3N0OjkwMDAvOwogICAgcHJveHlfc2V0X2hlYWRlciBIb3N0ICRob3N0OwogICAgYXV0aF9iYXNpYyAiUmVzdHJpY3RlZCBDb250ZW50IjsKICAgIGF1dGhfYmFzaWNfdXNlcl9maWxlIC9ldGMvbmdpbngvLmNlcmVicm9fYXV0aDsKICB9Cn0K"
+
+# Check for and remove old version of nginx setup files
+old_configs=("/etc/nginx/sites-available/cerebro" "/etc/nginx/sites-available/kibana" "/etc/nginx/sites-available/timesketch")
+for i in "${old_configs[@]}"
+do
+  sudo rm -f -- $i
+done
+
+# Configure default site
+echo $nginx_conf |base64 -d |sudo tee /etc/nginx/sites-available/default
+
+# Add domain name (if changed) and enable basic auth
+
+# Configure Kibana Credentials
+k_user="skadi"
+k_pass="skadi"
+echo $k_pass | sudo htpasswd -i -c /etc/nginx/.kibana_auth $k_user
+
+# Configure Cerebro Credentials
+c_user="skadi"
+c_pass="skadi"
+echo $c_pass | sudo htpasswd -i -c /etc/nginx/.cerebro_auth $c_user
+
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+# Install and Configure Letsencrypt
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt update -y
+sudo apt install python-certbot-nginx -y
+
+# Set default number of replicas to 0 (this prevents unassigned shards in default Skadi)
 curl -X PUT "localhost:9200/_template/all" -H 'Content-Type: application/json' -d'
 {
   "template": "*",
@@ -70,3 +139,34 @@ echo $c_pass | sudo htpasswd -i -c /etc/nginx/.cerebro_auth $c_user
 
 sudo systemctl restart nginx
 sudo systemctl enable nginx
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "The Nginx reverse proxy setup is complete with the following:"
+new_domain="`hostname or IP address`"
+echo "The following are now being reverse proxied with authentication at: "
+echo ""
+echo "  TimeSketch:"
+echo "   - 'http://$new_domain'"
+echo ""
+echo "  Kibana:"
+echo "   - 'http://$new_domain/kibana'"
+echo "     - Username: $k_user"
+echo "     - Password: $k_pass"
+echo ""
+echo "  Cerebro"
+echo "   - 'http://$new_domain/cerebro'"
+echo "     - Username: $c_user"
+echo "     - Password: $c_pass"
+echo ""
+echo ""
+echo "WARNING!!! Encryption is not enabled and it is strongly recommended to enable it before using on any type of production network"
+echo ""
+echo ""
+echo ""
+echo ""
+echo "The upgrade from 2018.1 to 2018.2 has been mostly completed"
+echo "All data in the ELK has been lost and the data in TimeSketch could be corrupted (due to the loss of Elasticsearch data) due to this upgrade"
+echo "Additionally, the `Post Installation` instructions from https://github.com/orlikoski/Skadi/wiki/Installation:-OpenSSL-Signed-Installation-Guide need to be followed"
--- scripts/secure_network.sh
+++ scripts/secure_network.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Installing Skadi Pack: Secure Networking"
 echo "This installation will do the following:"
-echo "  - Disable IPv6"
-echo "  - Install and configure Nginx reverse proxy for TimeSketch, Kibana, and Cerebro websites"
+echo "  - Update the Nginx reverse proxy for TimeSketch, Kibana, and Cerebro websites"
 echo "  - Install and configure all prerequisits to install valid TLS/SSL certificates from Letsencrypt"
-echo "  - Provide single command required to enable TLS/SSL encryption on all three websites"
+echo "  - Change the default passwords for TimeSketch, Kibana and Cerebro (created dynamically at run time)"
+echo "  - Enable TLS/SSL encryption using Letsencrypt"
 echo ""
-echo "In order to continue a hostname needs to provided and the Kibana, Cerebro, and TimeSketch websites will be setup as sub-domains"
-echo "This can be left blank for local use."
+echo "In order to continue a publicly accessable FQDN (such as 'mydomain.com') is required"
+echo "The Kibana, Cerebro, and TimeSketch websites will be configured to use that FQDN"
+echo "This cannot be left blank for the TLS certificates to work"
 echo ""
 echo ""
 echo "Example Domain: 'mydomain.com'"
@@ -24,68 +25,31 @@ echo "All usernames and passwords are made dynamically at run time"
 echo "These are displayed at the end of the script (record them for use)"
 echo ""
 echo "*********** WARNING ***********"
+echo "This script was built and tested to work on Skadi 2018.2"
+echo "It is not possible to predict the results of using it on any other platform"
 echo "root or sudo privileges are required for this installation"
 echo "*********** WARNING ***********"
 echo ""
 read -n 1 -r -s -p "Press any key to continue... or CTRL+C to exit (nothing has been installed)"
 echo ""
 echo ""
-# Disable IPv6
-echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
-sudo sysctl -p
 
 # Ask for and validate domain name to use
 echo ""
 echo ""
-read -p "Please enter the hostname name to use (leave blank if not using a FQDN or routable hostname): " new_domain
+read -p "Please enter the FQDN, IP address, or routable hostname to use (cannot be blank): " new_domain
 
 if [ -z "$new_domain" ]; then
-  echo "Warning: Domain entered was Null or empty"
-  echo "Using '_' for server name which will listen to all incoming requests"
-  echo "The server name can be changed later in /etc/nginx/sites-available/default"
+  echo "Warning: Hostname entered was Null or empty"
+  echo "This is required. Exiting"
+  exit
 fi
 
-# Install and configure gunicorn
-sudo pip2 install gunicorn
-sudo systemctl stop timesketch
-timesketch_service="W1VuaXRdCkRlc2NyaXB0aW9uPVRpbWVTa2V0Y2ggU2VydmljZQpBZnRlcj1uZXR3b3JrLnRhcmdldAoKW1NlcnZpY2VdClVzZXI9dGltZXNrZXRjaApHcm91cD10aW1lc2tldGNoCkV4ZWNTdGFydD0vdXNyL2xvY2FsL2Jpbi9ndW5pY29ybiAtLXdvcmtlcnMgNCAtLWJpbmQgMTI3LjAuMC4xOjUwMDAgdGltZXNrZXRjaC53c2dpCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK"
-echo $timesketch_service |base64 -d | sudo tee /etc/systemd/system/timesketch.service
-sudo systemctl daemon-reload
-sudo systemctl restart timesketch.service
-
-
-
-
-# Update Kibana to work with forwarding
-sudo systemctl stop kibana
-sudo sed -i "s@\#server.basePath: \"\"@server.basePath: \"/kibana\"@g" /etc/kibana/kibana.yml
-sudo systemctl start kibana
-
-# Install Nginx and web utils
-sudo apt install nginx apache2-utils -y
-sudo ufw allow 'Nginx Full'
-sudo ufw allow 'OpenSSH'
-sudo ufw --force enable
-
-# Configure Nginx for Kibana, Cerebro, and TimeSketch
-nginx_conf="c2VydmVyIHsKICBsaXN0ZW4gODA7CiAgc2VydmVyX25hbWUgXzsKICBjbGllbnRfbWF4X2JvZHlfc2l6ZSA3NU07CiAgcHJveHlfY29ubmVjdF90aW1lb3V0IDkwMHM7CiAgcHJveHlfcmVhZF90aW1lb3V0IDkwMHM7CiAgcm9vdCAgICAgICAgIC91c3Ivc2hhcmUvbmdpbngvaHRtbDsKICBlcnJvcl9wYWdlIDQwNCAvNDA0Lmh0bWw7CiAgICBsb2NhdGlvbiA9IC80MDQuaHRtbCB7fQogIGVycm9yX3BhZ2UgNTAwIDUwMiA1MDMgNTA0IC81MHguaHRtbDsKICAgIGxvY2F0aW9uID0gLzUweC5odG1sIHt9CgogIGVycm9yX2xvZyAgIC92YXIvbG9nL25naW54L2Vycm9yLmxvZzsKICBhY2Nlc3NfbG9nICAvdmFyL2xvZy9uZ2lueC9hY2Nlc3MubG9nOwoKICBsb2NhdGlvbiAvIHsKICAgIHByb3h5X3Bhc3MgaHR0cDovL2xvY2FsaG9zdDo1MDAwOwogICAgcHJveHlfaHR0cF92ZXJzaW9uIDEuMTsKICAgIHByb3h5X3NldF9oZWFkZXIgVXBncmFkZSAkaHR0cF91cGdyYWRlOwogICAgcHJveHlfc2V0X2hlYWRlciBDb25uZWN0aW9uICd1cGdyYWRlJzsKICAgIHByb3h5X3NldF9oZWFkZXIgSG9zdCAkaG9zdDsKICAgIHByb3h5X2NhY2hlX2J5cGFzcyAkaHR0cF91cGdyYWRlOwogICAgc3ViX2ZpbHRlciAnTG9nb3V0PC9hPicgJ0xvZ291dDwvYT48YnI+Jm5ic3A7Jm5ic3A7Jm5ic3A7PGEgdGFyZ2V0PSJfYmxhbmsiIHN0eWxlPSJjb2xvcjojZmZmOyIgaHJlZj0iL2tpYmFuYS8iPktpYmFuYTwvYT4mbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDs8YSB0YXJnZXQ9Il9ibGFuayIgc3R5bGU9ImNvbG9yOiNmZmY7IiBocmVmPSIvY2VyZWJyby8jL292ZXJ2aWV3P2hvc3Q9aHR0cDolMkYlMkZsb2NhbGhvc3Q6OTIwMCI+Q2VyZWJybzwvYT4nOwogICAgc3ViX2ZpbHRlciAnU2lnbiBpbjwvYnV0dG9uPicgJ1NpZ24gaW48L2J1dHRvbj48YnI+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9raWJhbmEvIj5LaWJhbmE8L2E+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9jZXJlYnJvLyMvb3ZlcnZpZXc/aG9zdD1odHRwOiUyRiUyRmxvY2FsaG9zdDo5MjAwIj5DZXJlYnJvPC9hPic7CiAgICBzdWJfZmlsdGVyX29uY2Ugb2ZmOwogIH0KCiAgbG9jYXRpb24gfiBeL2tpYmFuYSguKikkIHsKICAgIHByb3h5X2h0dHBfdmVyc2lvbiAxLjE7CiAgICBwcm94eV9zZXRfaGVhZGVyIFVwZ3JhZGUgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3NldF9oZWFkZXIgQ29ubmVjdGlvbiAndXBncmFkZSc7CiAgICBwcm94eV9zZXRfaGVhZGVyIEhvc3QgJGhvc3Q7CiAgICBwcm94eV9jYWNoZV9ieXBhc3MgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3Bhc3MgIGh0dHA6Ly9sb2NhbGhvc3Q6NTYwMTsKICAgIHJld3JpdGUgXi9raWJhbmEvKC4qKSQgLyQxIGJyZWFrOwogICAgcmV3cml0ZSBeL2tpYmFuYSQgL2tpYmFuYS87CiAgICBhdXRoX2Jhc2ljICJSZXN0cmljdGVkIENvbnRlbnQiOwogICAgYXV0aF9iYXNpY191c2VyX2ZpbGUgL2V0Yy9uZ2lueC8ua2liYW5hX2F1dGg7CiAgfQoKICBsb2NhdGlvbiAvY2VyZWJyby8gewogICAgcHJveHlfcGFzcyBodHRwOi8vbG9jYWxob3N0OjkwMDAvOwogICAgcHJveHlfc2V0X2hlYWRlciBIb3N0ICRob3N0OwogICAgYXV0aF9iYXNpYyAiUmVzdHJpY3RlZCBDb250ZW50IjsKICAgIGF1dGhfYmFzaWNfdXNlcl9maWxlIC9ldGMvbmdpbngvLmNlcmVicm9fYXV0aDsKICB9Cn0K"
-
-# Check for and remove old version of nginx setup files
-old_configs=("/etc/nginx/sites-available/cerebro" "/etc/nginx/sites-available/kibana" "/etc/nginx/sites-available/timesketch")
-for i in "${old_configs[@]}"
-do
-  sudo rm -f -- $i
-done
-
-# Configure default site
-echo $nginx_conf |base64 -d |sudo tee /etc/nginx/sites-available/default
 
 # Add domain name (if changed) and enable basic auth
 if [[ ! -z "$new_domain" ]]; then
-  echo "Replacing default server name '_' with '$new_domain'"
-  sudo sed -i "s/_\;/$new_domain\;/g" /etc/nginx/sites-available/default
+  echo "Replacing existing server name with '$new_domain'"
+  sudo sed -i "s/server_name .*\;/server_name $new_domain\;/g" /etc/nginx/sites-available/default
 fi
 
 # Configure Kibana Credentials
@@ -107,20 +71,16 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update -y
 sudo apt install python-certbot-nginx -y
+sudo apt autoremove -y
 
 echo ""
 echo ""
 echo ""
 echo ""
 echo ""
-echo "Nginx reverse proxy setup is complete with the following:"
-if [[ ! -z "$new_domain" ]]; then
-  echo "Domain: '$new_domain'"
-else
-  echo "Domain: <not set so listening to all requests>"
-  new_domain="exampledomain.com"
-fi
-echo "The following are now being reverse proxied with authentication at: "
+echo "Nginx reverse proxy update is complete with the following:"
+echo "Hostname: '$new_domain'"
+echo "The following are reverse proxied with authentication: "
 echo ""
 echo "  TimeSketch:"
 echo "   - 'http://$new_domain'"
@@ -136,9 +96,19 @@ echo "     - Username: $c_user"
 echo "     - Password: $c_pass"
 echo ""
 echo ""
-echo "WARNING!!! Encryption is not enabled and it is strongly recommended to enable it"
 echo ""
 echo ""
-echo "Letsencrypt is installed and able to encrypt these sites if a valid, internet routable FQDN is used. It is not enabled by default"
-echo "To start the Letsencrypt setup process type the following and follow the installation prompts:"
-echo "sudo certbot --nginx"
+echo "*********** WARNING ***********"
+echo "The next step requires a publicly accessable FQDN with working DNS"
+echo "If there is an issue with the FQDN, IP address, or routable hostname please stop now"
+echo " - This can be changed in '/etc/nginx/sites-available/default'"
+echo " - Change the line that start with 'server_name' to the correct name before continuing"
+echo ""
+echo "If there are any issues it is best to stop now and, when it is configured corrctly, run 'sudo certbot --nginx' manually"
+echo "*********** WARNING ***********"
+echo ""
+echo ""
+read -n 1 -r -s -p "If it is configured correctly; Press any key to continue... or CTRL+C to exit"
+echo ""
+echo ""
+sudo certbot --nginx
