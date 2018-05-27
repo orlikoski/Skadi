9274f681d8f76afa2c6643a95ed4667872ba943b
Alan Orlikoski <orlikoski@users.noreply.github.com>
2018-05-26 20:54:52 -0500

    Updated signedbuild

--- scripts/signedbuildskadi.sh
+++ scripts/signedbuildskadi.sh
@@ -72,7 +72,7 @@ sudo apt install mono-devel -y
 
 #Install and Configure Elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/5.5/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt update -y
 sudo apt dist-upgrade -y
@@ -172,7 +172,7 @@ sudo sed -i "s@NEO4J_PASSWORD = u''@NEO4J_PASSWORD = u'$neo4jpassword'@g" /etc/t
 
 
 timesketchpassword=$(openssl rand -base64 32)
-timesketchuser="cdqr_$(openssl rand -base64 3)"
+timesketchuser="skadi_$(openssl rand -base64 3)"
 tsctl add_user -u "$timesketchuser" -p "$timesketchpassword"
 
 timesketch_service="W1VuaXRdCkRlc2NyaXB0aW9uPVRpbWVTa2V0Y2ggU2VydmljZQpBZnRlcj1uZXR3b3JrLnRhcmdldAoKW1NlcnZpY2VdClVzZXI9dGltZXNrZXRjaApHcm91cD10aW1lc2tldGNoCkV4ZWNTdGFydD0vdXNyL2xvY2FsL2Jpbi90c2N0bCBydW5zZXJ2ZXIgLWggMC4wLjAuMCAtcCA1MDAwIC0tdGhyZWFkZWQgLS1wYXNzdGhyb3VnaC1lcnJvcnMgCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK"
@@ -252,6 +252,94 @@ rm /tmp/CyLR.zip
 echo ""
 echo ""
 
+# Disable IPv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.d/99-sysctl.conf
+sudo sysctl -p
+
+# Install and configure gunicorn
+sudo pip2 install gunicorn
+sudo systemctl stop timesketch
+timesketch_service="W1VuaXRdCkRlc2NyaXB0aW9uPVRpbWVTa2V0Y2ggU2VydmljZQpBZnRlcj1uZXR3b3JrLnRhcmdldAoKW1NlcnZpY2VdClVzZXI9dGltZXNrZXRjaApHcm91cD10aW1lc2tldGNoCkV4ZWNTdGFydD0vdXNyL2xvY2FsL2Jpbi9ndW5pY29ybiAtLXdvcmtlcnMgNCAtLWJpbmQgMTI3LjAuMC4xOjUwMDAgdGltZXNrZXRjaC53c2dpCgpbSW5zdGFsbF0KV2FudGVkQnk9bXVsdGktdXNlci50YXJnZXQK"
+echo $timesketch_service |base64 -d | sudo tee /etc/systemd/system/timesketch.service
+sudo systemctl daemon-reload
+sudo systemctl restart timesketch.service
+
+# Update Kibana to work with forwarding
+sudo systemctl stop kibana
+sudo sed -i "s@\#server.basePath: \"\"@server.basePath: \"/kibana\"@g" /etc/kibana/kibana.yml
+sudo systemctl start kibana
+
+# Install Nginx and web utils
+sudo apt install nginx apache2-utils -y
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
+sudo ufw --force enable
+
+# Configure Nginx for Kibana, Cerebro, and TimeSketch
+nginx_conf="c2VydmVyIHsKICBsaXN0ZW4gODA7CiAgc2VydmVyX25hbWUgXzsKICBjbGllbnRfbWF4X2JvZHlfc2l6ZSA3NU07CiAgcHJveHlfY29ubmVjdF90aW1lb3V0IDkwMHM7CiAgcHJveHlfcmVhZF90aW1lb3V0IDkwMHM7CiAgcm9vdCAgICAgICAgIC91c3Ivc2hhcmUvbmdpbngvaHRtbDsKICBlcnJvcl9wYWdlIDQwNCAvNDA0Lmh0bWw7CiAgICBsb2NhdGlvbiA9IC80MDQuaHRtbCB7fQogIGVycm9yX3BhZ2UgNTAwIDUwMiA1MDMgNTA0IC81MHguaHRtbDsKICAgIGxvY2F0aW9uID0gLzUweC5odG1sIHt9CgogIGVycm9yX2xvZyAgIC92YXIvbG9nL25naW54L2Vycm9yLmxvZzsKICBhY2Nlc3NfbG9nICAvdmFyL2xvZy9uZ2lueC9hY2Nlc3MubG9nOwoKICBsb2NhdGlvbiAvIHsKICAgIHByb3h5X3Bhc3MgaHR0cDovL2xvY2FsaG9zdDo1MDAwOwogICAgcHJveHlfaHR0cF92ZXJzaW9uIDEuMTsKICAgIHByb3h5X3NldF9oZWFkZXIgVXBncmFkZSAkaHR0cF91cGdyYWRlOwogICAgcHJveHlfc2V0X2hlYWRlciBDb25uZWN0aW9uICd1cGdyYWRlJzsKICAgIHByb3h5X3NldF9oZWFkZXIgSG9zdCAkaG9zdDsKICAgIHByb3h5X2NhY2hlX2J5cGFzcyAkaHR0cF91cGdyYWRlOwogICAgc3ViX2ZpbHRlciAnTG9nb3V0PC9hPicgJ0xvZ291dDwvYT48YnI+Jm5ic3A7Jm5ic3A7Jm5ic3A7PGEgdGFyZ2V0PSJfYmxhbmsiIHN0eWxlPSJjb2xvcjojZmZmOyIgaHJlZj0iL2tpYmFuYS8iPktpYmFuYTwvYT4mbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDsmbmJzcDs8YSB0YXJnZXQ9Il9ibGFuayIgc3R5bGU9ImNvbG9yOiNmZmY7IiBocmVmPSIvY2VyZWJyby8jL292ZXJ2aWV3P2hvc3Q9aHR0cDolMkYlMkZsb2NhbGhvc3Q6OTIwMCI+Q2VyZWJybzwvYT4nOwogICAgc3ViX2ZpbHRlciAnU2lnbiBpbjwvYnV0dG9uPicgJ1NpZ24gaW48L2J1dHRvbj48YnI+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9raWJhbmEvIj5LaWJhbmE8L2E+PGJyPjxhIHRhcmdldD0iX2JsYW5rIiBzdHlsZT0iY29sb3I6I2ZmZjsiIGhyZWY9Ii9jZXJlYnJvLyMvb3ZlcnZpZXc/aG9zdD1odHRwOiUyRiUyRmxvY2FsaG9zdDo5MjAwIj5DZXJlYnJvPC9hPic7CiAgICBzdWJfZmlsdGVyX29uY2Ugb2ZmOwogIH0KCiAgbG9jYXRpb24gfiBeL2tpYmFuYSguKikkIHsKICAgIHByb3h5X2h0dHBfdmVyc2lvbiAxLjE7CiAgICBwcm94eV9zZXRfaGVhZGVyIFVwZ3JhZGUgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3NldF9oZWFkZXIgQ29ubmVjdGlvbiAndXBncmFkZSc7CiAgICBwcm94eV9zZXRfaGVhZGVyIEhvc3QgJGhvc3Q7CiAgICBwcm94eV9jYWNoZV9ieXBhc3MgJGh0dHBfdXBncmFkZTsKICAgIHByb3h5X3Bhc3MgIGh0dHA6Ly9sb2NhbGhvc3Q6NTYwMTsKICAgIHJld3JpdGUgXi9raWJhbmEvKC4qKSQgLyQxIGJyZWFrOwogICAgcmV3cml0ZSBeL2tpYmFuYSQgL2tpYmFuYS87CiAgICBhdXRoX2Jhc2ljICJSZXN0cmljdGVkIENvbnRlbnQiOwogICAgYXV0aF9iYXNpY191c2VyX2ZpbGUgL2V0Yy9uZ2lueC8ua2liYW5hX2F1dGg7CiAgfQoKICBsb2NhdGlvbiAvY2VyZWJyby8gewogICAgcHJveHlfcGFzcyBodHRwOi8vbG9jYWxob3N0OjkwMDAvOwogICAgcHJveHlfc2V0X2hlYWRlciBIb3N0ICRob3N0OwogICAgYXV0aF9iYXNpYyAiUmVzdHJpY3RlZCBDb250ZW50IjsKICAgIGF1dGhfYmFzaWNfdXNlcl9maWxlIC9ldGMvbmdpbngvLmNlcmVicm9fYXV0aDsKICB9Cn0K"
+
+
+# Configure default site
+echo $nginx_conf |base64 -d |sudo tee /etc/nginx/sites-available/default
+
+# Configure Kibana Credentials
+k_user="kibuser_$(openssl rand -base64 3)"
+k_pass=$(openssl rand -base64 32)
+echo $k_pass | sudo htpasswd -i -c /etc/nginx/.kibana_auth $k_user
+
+# Configure Cerebro Credentials
+c_user="ceruser_$(openssl rand -base64 3)"
+c_pass=$(openssl rand -base64 32)
+echo $c_pass | sudo htpasswd -i -c /etc/nginx/.cerebro_auth $c_user
+
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+# Install and Configure Letsencrypt
+sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt update -y
+sudo apt install python-certbot-nginx -y
+
+new_domain="Hostname or IP Address"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Nginx reverse proxy setup is complete with the following:"
+echo "Hostname: '$new_domain'"
+echo "The following are now being reverse proxied with authentication at: "
+echo ""
+echo "  TimeSketch:"
+echo "   - 'http://$new_domain'"
+echo "     - Username: $timesketchuser"
+echo "     - Password: $timesketchpassword"
+echo ""
+echo "  Kibana:"
+echo "   - 'http://$new_domain/kibana'"
+echo "     - Username: $k_user"
+echo "     - Password: $k_pass"
+echo ""
+echo "  Cerebro"
+echo "   - 'http://$new_domain/cerebro'"
+echo "     - Username: $c_user"
+echo "     - Password: $c_pass"
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo ""
+echo "Logstash and automation_grpc are installed but not enabled by default"
+echo "To enable run the following commands"
+echo "    sudo systemctl restart logstash automation_grpc_service"
+echo "    sudo systemctl enable logstash automation_grpc_service"
+echo ""
+echo ""
 clear
 echo "Installed Software Version Checks (Where it is supported)"
 /usr/bin/log2timeline.py --version
@@ -283,15 +371,4 @@ for item in "${services[@]}"
 do
     echo "  $item service is: $(systemctl is-active $item)"
 done
-
-echo ""
-echo "Logstash and automation_grpc are installed but not enabled by default"
-echo "To enable run the following commands"
-echo "    sudo systemctl restart logstash automation_grpc_service"
-echo "    sudo systemctl enable logstash automation_grpc_service"
-echo ""
-echo ""
-echo "Now installing secure networking pack"
-wget -O /tmp/secure_network.sh https://raw.githubusercontent.com/orlikoski/Skadi/master/scripts/secure_network.sh
-bash /tmp/secure_network.sh
 exec bash
