f3c23970d0b85670a756b8aabad14141fad1f9c5
@AlanOrlikoski <9388572+orlikoski@users.noreply.github.com>
2018-09-25 08:52:49 -0500

    Skadi 2018.4 Updates

--- Packer/script/virtualbox.sh
+++ Packer/script/virtualbox.sh
@@ -1,21 +1,17 @@
 #!/bin/bash -eux
 
-SSH_USER=${SSH_USERNAME:-vagrant}
+#SSH_USER=${SSH_USERNAME:-vagrant}
+SSH_USER="root"
 
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-    echo "==> Installing VirtualBox guest additions"
-    # Assuming the following packages are installed
-    # apt-get install -y linux-headers-$(uname -r) build-essential perl
-    # apt-get install -y dkms
 
-    VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
-    mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-    sh /mnt/VBoxLinuxAdditions.run
-    umount /mnt
-    rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
-    rm /home/${SSH_USER}/.vbox_version
+echo "==> Installing VirtualBox guest additions"
+# Assuming the following packages are installed
+# apt-get install -y linux-headers-$(uname -r) build-essential perl
+# apt-get install -y dkms
 
-    if [[ $VBOX_VERSION = "4.3.10" ]]; then
-        ln -s /opt/VBoxGuestAdditions-4.3.10/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
-    fi
-fi
+#VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
+mount -o loop /${SSH_USER}/VBoxGuestAdditions.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm /${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
+rm /${SSH_USER}/.vbox_version
