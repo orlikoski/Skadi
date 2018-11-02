2278ec67f33d5b6ddb44e33b27cbe14968aa154c
Alan Orlikoski <alan@orlikoski.com>
2018-11-02 14:18:25 -0500

    CyLR update script setup for v2.0 version

--- scripts/update.sh
+++ scripts/update.sh
@@ -14,43 +14,48 @@ sudo mv /tmp/cdqr.py /usr/local/bin/cdqr.py
 echo "CDQR is in /usr/local/bin/cdqr.py"
 
 echo "Updating CyLR"
-#Building the CyLR link
+# Building the CyLR link
+cylr_files=( "CyLR_linux-x64.zip" "CyLR_osx-x64.zip" "CyLR_win-x64.zip" "CyLR_win-x86.zip")
 LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/orlikoski/CyLR/releases/latest)
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-ARTIFACT_URL="https://github.com/orlikoski/CyLR/releases/download/$LATEST_VERSION/CyLR.zip"
+ARTIFACT_URL="https://github.com/orlikoski/CyLR/releases/download/$LATEST_VERSION/"
 
-wget -O /tmp/CyLR.zip $ARTIFACT_URL
-if [ ! -d "/opt/CyLR" ]; then
-  sudo mkdir /opt/CyLR/
-  sudo chmod 777 /opt/CyLR
-else
-  sudo rm -rf /opt/CyLR/*
+# Remove old versions
+if [ -f /opt/CyLR/CyLR.exe ]; then
+    sudo rm /opt/CyLR/CyLR.exe
 fi
-if [ -d "CyLR/" ]; then
-  sudo rm -rf CyLR/
-fi
-
-unzip /tmp/CyLR.zip -d /opt/CyLR/
-if [ $? -eq 0 ]; then
-  echo "CyLR installed into /opt/CyLR/"
-else
-  echo "Error, install unzip and try again"
-  sudo apt install unzip -y
-  unzip /tmp/CyLR.zip -d /opt/CyLR/
-  if [ $? -ne 0 ]; then
-    echo "CyLR Update failed"
-  else
-    echo "CyLR is in /opt/CyLR/"
-  fi
-fi
-
-# If CyLR.exe is on the Desktop, update it
 if [ -f /home/skadi/Desktop/CyLR.exe ]; then
     sudo rm /home/skadi/Desktop/CyLR.exe
-    sudo cp /opt/CyLR/CyLR/CyLR.exe /home/skadi/Desktop/CyLR.exe
-    sudo chown skadi:skadi /home/skadi/Desktop/CyLR.exe
 fi
 
-rm /tmp/CyLR.zip
-echo "CyLR successfully updated in /opt/CyLR/"
+for cylrzip in "${cylr_files[@]}"
+do
+  if [ ! -d "/opt/CyLR" ]; then
+    sudo mkdir /opt/CyLR/
+    sudo chmod 777 /opt/CyLR
+  else
+    sudo rm -rf /opt/CyLR/$cylrzip
+  fi
+  wget -O "/opt/CyLR/$cylrzip" "$ARTIFACT_URL/$cylrzip" > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo "CyLR Download of $cylrzip failed"
+  else
+      if [ -d "CyLR/" ]; then
+        sudo rm -rf CyLR/
+      fi
+      echo "$cylrzip downloaded into /opt/CyLR/"
+  fi
+done
+# If Skadi Desktop exists place link to CyLR folder on it
+if [ -d /home/skadi/Desktop ]; then
+    sudo ln -s /opt/CyLR /home/skadi/Desktop/CyLR
+    sudo chown -h skadi:skadi /home/skadi/Desktop/CyLR
+fi
+
+unzip -o /opt/CyLR/CyLR_linux-x64.zip -d /tmp/ > /dev/null 2>&1
+cylr_version=$(/tmp/CyLR --version |grep Version)
+rm /tmp/CyLR > /dev/null 2>&1
+echo "All CyLR Files Downloaded"
+echo "Updated to $cylr_version"
+
 echo "Update successful"
