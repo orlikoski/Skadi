bc356dad38dbf0e65980f397f8978797b34a9a71
Luke <lkuik@shaw.ca>
2019-01-13 00:09:32 +0000

    updating echo statements and removing letsencrypt

--- scripts/secure_network.sh
+++ scripts/secure_network.sh
@@ -2,25 +2,17 @@
 
 echo "Installing Skadi Pack: Secure Networking"
 echo "This installation will do the following:"
-echo "  - Update the Nginx reverse proxy for TimeSketch, Kibana, and Cerebro websites"
-echo "  - Install and configure all prerequisits to install valid TLS/SSL certificates from Letsencrypt"
+echo "  - Install and configure all prerequisites for mkcert to issue valid self-signed TLS certs"
 echo "  - Change the default passwords for TimeSketch, Kibana and Cerebro (created dynamically at run time)"
-echo "  - Enable TLS/SSL encryption using Letsencrypt"
 echo ""
-echo "In order to continue a publicly accessable FQDN (such as 'mydomain.com') is required"
-echo "The Kibana, Cerebro, and TimeSketch websites will be configured to use that FQDN"
-echo "This cannot be left blank for the TLS certificates to work"
+echo "Mkcert is installed and configured to do the following:"
+echo "  - Install to local keystore"
+echo "  - Generate certs for hostinfo, localhost, 127.0.0.1, and ::1"
+echo "  - Write these certs to nginx config to serve nginx with TLS encryption"
+echo " 
+echo "PLEASE NOTE: BROWSERS WILL STILL SHOW AS NOT SECURE EVEN THOUGH THEY ARE SERVING TLS ENCRYPTION"
 echo ""
-echo ""
-echo "Example Domain: 'mydomain.com'"
-echo "Results in the following: "
-echo "   - 'mydomain.com'"
-echo "   - 'mydomain.com/kibana'"
-echo "   - 'mydomain.com/cerebro'"
-echo ""
-echo "All of this can be changed in the following file:"
-echo "   - /etc/nginx/sites-available/default"
-echo ""
+echo "For further info on self-signed certs please see: https://github.com/FiloSottile/mkcert"
 echo ""
 echo "All usernames and passwords are made dynamically at run time"
 echo "These are displayed at the end of the script (record them for use)"
@@ -50,6 +42,8 @@ fi
 # Add domain name (if changed) and enable basic auth
 if [[ ! -z "$hostinfo" ]]; then
   echo "Replacing existing server name with '$hostinfo'"
+
+# Set up Environment for nginx TLS config
 sudo mkdir -p /etc/nginx/certs/
 sudo rm -rf /etc/nginx/conf.d/*
 cat /opt/Skadi/Docker/nginx/skadi_TLS.conf | sed "s/server_name  localhost/server_name $hostinfo localhost 127.0.0.1 ::1/g" > /tmp/skadi_TLS.conf
@@ -59,7 +53,13 @@ sudo mv /tmp/skadi_TLS.conf /etc/nginx/conf.d/
 sudo chown root:root /etc/nginx/conf.d/skadi_TLS.conf
 sudo chmod 644 /etc/nginx/conf.d/skadi_TLS.conf
 fi
-
+echo "Nginx configuration for enabling TLS has been updated"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
 # Install and Configure Mkcert
 sudo apt update -y
 sudo apt-get install libnss3-tools -y
@@ -67,14 +67,12 @@ VER="v1.2.0"
 sudo wget -O /usr/local/bin/mkcert "https://github.com/FiloSottile/mkcert/releases/download/$VER/mkcert-$VER-linux-amd64"
 sudo chmod +x /usr/local/bin/mkcert
 sudo mkcert -install -cert-file /etc/nginx/certs/$hostinfo.pem -key-file /etc/nginx/certs/$hostinfo.key.pem 127.0.0.1 localhost $hostinfo ::1
-
-# Install and Configure Letsencrypt
-#sudo apt update -y
-#sudo apt install software-properties-common -y
-#sudo add-apt-repository ppa:certbot/certbot -y
-#sudo apt update -y
-#sudo apt install python-certbot-nginx -y
-#sudo apt autoremove -y
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Mkcert has been installed and written generated certs to nginx certs directory"
 echo ""
 echo ""
 echo ""
@@ -100,19 +98,19 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo "*********** WARNING ***********"
-echo "The next step requires a publicly accessable FQDN with working DNS"
-echo "If there is an issue with the FQDN, IP address, or routable hostname please stop now"
-echo " - This can be changed in '/etc/nginx/sites-available/default'"
-echo " - Change the line that start with 'server_name' to the correct name before continuing"
-echo ""
-echo "If there are any issues it is best to stop now and, when it is configured corrctly, run 'sudo certbot --nginx' manually"
-echo "*********** WARNING ***********"
-echo ""
 echo ""
 read -n 1 -r -s -p "If it is configured correctly; Press any key to continue... or CTRL+C to exit"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+# Restarting docker
 cd /opt/Skadi/Docker
 sudo docker-compose up -d
 echo ""
 echo ""
-#sudo certbot --nginx
+echo ""
+echo ""
+echo ""
+echo "Docker containers have been brought back up to take the above changes"
