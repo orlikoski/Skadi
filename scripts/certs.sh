30ea21cae4471b80d05756c094008a23e46e5109
Alan Orlikoski <alan@orlikoski.com>
2019-01-13 13:00:49 -0600

    Using the alpine image that is supported going forward

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -4,7 +4,7 @@ sudo docker run -it --rm --name certbot \
   -v /etc/nginx/certs/letsencrypt:/etc/letsencrypt \
   -v /var/log/letsencrypt:/var/log/letsencrypt \
   -v /usr/share/nginx/html/letsencrypt:/var/www/.well-known \
-  quay.io/letsencrypt/letsencrypt -t certonly \
+  certbot/certbot -t certonly \
   --agree-tos --renew-by-default \
   --webroot -w /var/www \
   -d localhost
