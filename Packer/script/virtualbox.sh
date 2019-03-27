67ec0bfae16306c959c62e169ff2ef994dc43bc3
Alan Orlikoski <alan@orlikoski.com>
2019-03-26 22:33:40 -0500

    Updating Build

--- Packer/script/virtualbox.sh
+++ Packer/script/virtualbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-SSH_USER=${SSH_USERNAME:-skadi}
+SSH_USER=skadi
 #SSH_USER="root"
 
 
@@ -9,9 +9,9 @@ echo "==> Installing VirtualBox guest additions"
 #apt-get install -y linux-headers-$(uname -r) build-essential perl
 #apt-get install -y dkms
 
-VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
-mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+VBOX_VERSION=$(cat /home/$SSH_USER/.vbox_version)
+mount -o loop /home/$SSH_USER/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
-rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
-rm /home/${SSH_USER}/.vbox_version
+rm /home/$SSH_USER/VBoxGuestAdditions_$VBOX_VERSION.iso
+rm /home/$SSH_USER/.vbox_version
