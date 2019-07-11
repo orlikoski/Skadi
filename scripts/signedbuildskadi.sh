7180ec1ba4aaac60a446e9d8e7999e74794010c4
Luke <lkuik@shaw.ca>
2019-07-11 13:46:26 -0500

    updating signedbuildskadi.sh to make functions more clear

--- scripts/signedbuildskadi.sh
+++ scripts/signedbuildskadi.sh
@@ -1,223 +1,251 @@
 #!/bin/bash
 set -e
 
-# Choosing to use default passwords or not
-default_skadi_passwords=${DEFAULT_PASSWORDS:-false}
+hello_message () {
+  echo "Welcome to installing a secure dockerized container setup of Skadi"
+  echo "Please ensure you have at least 8 GB RAM and 4 cores allocated to the host"
+  read -n 1 -r -s -p "If you already have this configured press any key to continue... or CTRL+C to exit (nothing has been installed)"
+  echo ""
+}
 
-# Set the installation branch
-install_branch=${INSTALL_BRANCH:-"master"}
+setup_host () {
+  # Update
+  sudo apt-get update && sudo apt-get dist-upgrade -y
 
-# Set the value for if the hostname should be changed
-hostname_change=${SKADI_HOSTNAME:-true}
+  # Install deps
+  sudo apt-get install -y \
+    openssh-server \
+    git \
+    curl \
+    glances \
+    unzip \
+    vim \
+    htop \
+    screen \
+    gnupg \
+    net-tools \
+    software-properties-common \
+    apache2-utils
 
-# Set the value for if the skadi user should be created
-create_skadi_user=${MAKE_SKADI_USER:-true}
+  # Set the vm.max_map_count kernel setting needs to be set to at least 262144 for production use
+    sudo sysctl -w vm.max_map_count=262144
+    echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf
 
-# Set the value for if the server time should be set to UTC
-set_time_utc=${UTC_TIME:-true}
+  # Set Hostname to skadi by default with option to not to
+    if [ $hostname_change = "true" ]
+      then
+      echo "Renaming Host to skadi"
+      newhostname='skadi'
+      oldhostname=$(</etc/hostname)
+      sudo hostname $newhostname >/dev/null 2>&1
+      sudo sed -i "s/$oldhostname/$newhostname/g" /etc/hosts >/dev/null 2>&1
+      echo skadi |sudo tee /etc/hostname >/dev/null 2>&1
+      sudo systemctl restart systemd-logind.service >/dev/null 2>&1
+    else
+      echo "Not renaming host"
+    fi
 
-# Default Values
-SKADI_USER="skadi"
-SKADI_PASS="skadi"
-SKADI_USER_HOME="/home/$SKADI_USER"
-TIMESKETCH_USER="skadi"
-TIMESKETCH_PASSWORD="skadi"
-NGINX_USER="skadi"
-NGINX_PASSWORD="skadi"
-GRAFANA_USER=$NGINX_USER
-GRAFANA_PASSWORD=$NGINX_PASSWORD
+    # Set Server to UTC or not
+    if [ $set_time_utc = "true" ]
+      then
+      # Set Timezone to UTC
+      sudo timedatectl set-timezone UTC
+    fi
 
-# Update
-sudo apt-get update && sudo apt-get dist-upgrade -y
+  # Create Skadi user by default with option to not to
+  if [ $create_skadi_user = "true" ]
+    then
+    if ! id -u $SKADI_USER >/dev/null 2>&1; then
+        echo "==> Creating $SKADI_USER user"
+        echo "" >> /opt/skadi_credentials
+        echo "  Created OS Account:" >> /opt/skadi_credentials
+        echo "     - Username: $SKADI_USER" >> /opt/skadi_credentials
+        echo "     - Password: $SKADI_PASS" >> /opt/skadi_credentials
+        /usr/sbin/groupadd $SKADI_USER
+        /usr/sbin/useradd $SKADI_USER -g $SKADI_USER -G sudo -d $SKADI_USER_HOME --create-home -s "/bin/bash"
+        echo "$SKADI_USER:$SKADI_PASS" | chpasswd
 
-# Install deps
-sudo apt-get install -y \
-  openssh-server \
-  git \
-  curl \
-  glances \
-  unzip \
-  vim \
-  htop \
-  screen \
-  gnupg \
-  net-tools \
-  software-properties-common \
-  apache2-utils
-
-# Add Docker gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-# Install Docker
-sudo apt-get update
-sudo apt-get install docker-ce -y
-sudo systemctl enable docker
-
-# Install Docker-Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-
-# Clean APT
-sudo apt-get -y autoremove --purge
-sudo apt-get -y clean
-sudo apt-get -y autoclean
-
-# Set Credentials
-SECRET_KEY=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
-POSTGRES_USER="timesketch"
-psql_pw=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
-neo4juser='neo4j'
-
-# Set Hostname to skadi by default with option to not to
-if [ $hostname_change = "true" ]
-  then
-  echo "Renaming Host to skadi"
-  newhostname='skadi'
-  oldhostname=$(</etc/hostname)
-  sudo hostname $newhostname >/dev/null 2>&1
-  sudo sed -i "s/$oldhostname/$newhostname/g" /etc/hosts >/dev/null 2>&1
-  echo skadi |sudo tee /etc/hostname >/dev/null 2>&1
-  sudo systemctl restart systemd-logind.service >/dev/null 2>&1
-else
-  echo "Not renaming host"
-fi
-
-# Use dynamically created passwords with an option to use defaults
-if [ $default_skadi_passwords = "false" ]
-  then
-    echo "Using random username and passwords for OS Account, TimeSketch, Nginx proxy / Grafana"
-    echo "Writing all credentials to /opt/skadi_credentials"
-    TIMESKETCH_USER="skadi_$(openssl rand -base64 3)"
-    TIMESKETCH_PASSWORD=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
-    NGINX_USER="skadi_$(openssl rand -base64 3)"
-    NGINX_PASSWORD=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
-    GRAFANA_USER=$NGINX_USER
-    GRAFANA_PASSWORD=$NGINX_PASSWORD
-    SKADI_PASS=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
-    SKADI_USER_HOME="/home/$SKADI_USER"
-    echo "  Proxy & Grafana Account:" > /opt/skadi_credentials
-    echo "     - Username: $NGINX_USER" >> /opt/skadi_credentials
-    echo "     - Password: $NGINX_PASSWORD" >> /opt/skadi_credentials
-    echo "" >> /opt/skadi_credentials
-    echo "  TimeSketch Account:" >> /opt/skadi_credentials
-    echo "     - Username: $TIMESKETCH_USER" >> /opt/skadi_credentials
-    echo "     - Password: $TIMESKETCH_PASSWORD" >> /opt/skadi_credentials
-else
-    echo "Using Skadi default username and password of skadi:skadi for OS Account, TimeSketch, Nginx proxy, and Grafana"
-fi
-
-# Create Skadi user by default with option to not to
-if [ $create_skadi_user = "true" ]
-  then
-  if ! id -u $SKADI_USER >/dev/null 2>&1; then
-      echo "==> Creating $SKADI_USER user"
-      echo "" >> /opt/skadi_credentials
-      echo "  Created OS Account:" >> /opt/skadi_credentials
-      echo "     - Username: $SKADI_USER" >> /opt/skadi_credentials
-      echo "     - Password: $SKADI_PASS" >> /opt/skadi_credentials
-      /usr/sbin/groupadd $SKADI_USER
-      /usr/sbin/useradd $SKADI_USER -g $SKADI_USER -G sudo -d $SKADI_USER_HOME --create-home -s "/bin/bash"
-      echo "$SKADI_USER:$SKADI_PASS" | chpasswd
-
-      # Set up sudo
-      echo "==> Giving $SKADI_USER sudo powers"
-      echo "$SKADI_USER        ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/$SKADI_USER
-      chmod 440 /etc/sudoers.d/$SKADI_USER
+        # Set up sudo
+        echo "==> Giving $SKADI_USER sudo powers"
+        echo "$SKADI_USER        ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/$SKADI_USER
+        chmod 440 /etc/sudoers.d/$SKADI_USER
+    fi
+  else
+    echo "Not creating skadi user"
+    SKADI_USER=$(whoami)
   fi
-else
-  echo "Not creating skadi user"
-  SKADI_USER=$(whoami)
-fi
 
-# Create needed folders
-sudo mkdir -p /etc/nginx/conf.d
-sudo mkdir -p /usr/share/nginx/html
+  # Enable and Configure UFW Firewall
+  echo "Enabling UFW firewall to only allow OpenSSH and Ngninx Full"
+  sudo ufw allow 80
+  sudo ufw allow 443
+  sudo ufw allow 'OpenSSH'
+  sudo ufw --force enable
+}
 
-# Copy Nginx configuration files to required locations
-sudo git clone --single-branch --branch $install_branch https://github.com/orlikoski/Skadi.git /opt/Skadi
-sudo chown -R $SKADI_USER:$SKADI_USER /opt/Skadi
-sudo cp /opt/Skadi/Docker/nginx/skadi_default.conf /etc/nginx/conf.d
-sudo cp -r /opt/Skadi/Docker/nginx/www/* /usr/share/nginx/html
+host_default_values () {
+  # Choosing to use default passwords or not
+  default_skadi_passwords=${DEFAULT_PASSWORDS:-false}
 
-# Copy cdqr script to /usr/local/bin
-sudo cp /opt/Skadi/scripts/cdqr /usr/local/bin/cdqr
-sudo chmod +x /usr/local/bin/cdqr
+  # Set the installation branch
+  install_branch=${INSTALL_BRANCH:-"master"}
 
-# Setup Nginx Auth
-echo $NGINX_PASSWORD | sudo htpasswd -i -c /etc/nginx/.skadi_auth $NGINX_USER
+  # Set the value for if the hostname should be changed
+  hostname_change=${SKADI_HOSTNAME:-true}
 
-# Set Server to UTC or not
-if [ $set_time_utc = "true" ]
-  then
-  # Set Timezone to UTC
-  sudo timedatectl set-timezone UTC
-fi
+  # Set the value for if the skadi user should be created
+  create_skadi_user=${MAKE_SKADI_USER:-true}
 
-# Create CyLR directory
-sudo mkdir /opt/CyLR/
-sudo chmod 777 /opt/CyLR
+  # Set the value for if the server time should be set to UTC
+  set_time_utc=${UTC_TIME:-true}
 
-# Add skadi to docker usergroup
-sudo usermod -aG docker $SKADI_USER
+  # Default Values
+ SKADI_USER="skadi"
+ SKADI_PASS="skadi"
+ SKADI_USER_HOME="/home/$SKADI_USER"
+ TIMESKETCH_USER="skadi"
+ TIMESKETCH_PASSWORD="skadi"
+ NGINX_USER="skadi"
+ NGINX_PASSWORD="skadi"
+ GRAFANA_USER=$NGINX_USER
+ GRAFANA_PASSWORD=$NGINX_PASSWORD
+}
 
-# Set the vm.max_map_count kernel setting needs to be set to at least 262144 for production use
-sudo sysctl -w vm.max_map_count=262144
-echo vm.max_map_count=262144 | sudo tee -a /etc/sysctl.conf
+setup_credentials () {
+  # Set Credentials
+  SECRET_KEY=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
+  POSTGRES_USER="timesketch"
+  psql_pw=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
+  neo4juser='neo4j'
 
-# Write TS and Postgres creds to .env file
-cd /opt/Skadi/Docker/
-echo TIMESKETCH_USER=$TIMESKETCH_USER > ./.env
-echo TIMESKETCH_PASSWORD=$TIMESKETCH_PASSWORD >> ./.env
-echo POSTGRES_USER=$POSTGRES_USER >> ./.env
-echo POSTGRES_PASSWORD=$psql_pw >> ./.env
-echo HEAP_SIZE=1g >> ./.env
+  # Use dynamically created passwords with an option to use defaults
+  if [ $default_skadi_passwords = "false" ]
+    then
+      echo "Using random username and passwords for OS Account, TimeSketch, Nginx proxy / Grafana"
+      echo "Writing all credentials to /opt/skadi_credentials"
+      TIMESKETCH_USER="skadi_$(openssl rand -base64 3)"
+      TIMESKETCH_PASSWORD=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
+      NGINX_USER="skadi_$(openssl rand -base64 3)"
+      NGINX_PASSWORD=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
+      GRAFANA_USER=$NGINX_USER
+      GRAFANA_PASSWORD=$NGINX_PASSWORD
+      SKADI_PASS=$(openssl rand -base64 32 |sha256sum | sed 's/ //g')
+      SKADI_USER_HOME="/home/$SKADI_USER"
+      echo "  Proxy & Grafana Account:" > /opt/skadi_credentials
+      echo "     - Username: $NGINX_USER" >> /opt/skadi_credentials
+      echo "     - Password: $NGINX_PASSWORD" >> /opt/skadi_credentials
+      echo "" >> /opt/skadi_credentials
+      echo "  TimeSketch Account:" >> /opt/skadi_credentials
+      echo "     - Username: $TIMESKETCH_USER" >> /opt/skadi_credentials
+      echo "     - Password: $TIMESKETCH_PASSWORD" >> /opt/skadi_credentials
+  else
+      echo "Using Skadi default username and password of skadi:skadi for OS Account, TimeSketch, Nginx proxy, and Grafana"
+  fi
+}
 
-# Configure /etc/hosts file so the host can use same names for each service as the TimeSketch Dockers
-echo 127.0.0.1       elasticsearch |sudo tee -a /etc/hosts
-echo 127.0.0.1       postgres |sudo tee -a /etc/hosts
-echo 127.0.0.1       neo4j |sudo tee -a /etc/hosts
-echo 127.0.0.1       redis |sudo tee -a /etc/hosts
+setup_docker () {
+  # Add Docker gpg key
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-# Write TimeSketch config file on host
-sudo cp /opt/Skadi/Docker/timesketch/timesketch.conf /etc/
-sudo sed -i "s@SECRET_KEY = '<KEY_GOES_HERE>'@SECRET_KEY = '$SECRET_KEY'@g" /etc/timesketch.conf
-sudo sed -i "s@<USERNAME>\:<PASSWORD>@$POSTGRES_USER\:$psql_pw@g" /etc/timesketch.conf
-sudo sed -i "s@NEO4J_USERNAME = 'neo4j'@NEO4J_USERNAME = '$neo4juser'@g" /etc/timesketch.conf
-sudo sed -i "s@NEO4J_PASSWORD = '<NEO4J_PASSWORD>'@NEO4J_PASSWORD = ''@g" /etc/timesketch.conf
-sudo sed -i "s/UPLOAD_ENABLED = False/UPLOAD_ENABLED = True/g" /etc/timesketch.conf
-sudo sed -i "s/GRAPH_BACKEND_ENABLED = False/GRAPH_BACKEND_ENABLED = True/g" /etc/timesketch.conf
-sudo sed -i "s#@localhost/timesketch#@postgres/timesketch#g" /etc/timesketch.conf
-sudo sed -i "s/ELASTIC_HOST = '127.0.0.1'/ELASTIC_HOST = 'elasticsearch'/g" /etc/timesketch.conf
-sudo sed -i "s@'redis://127.0.0.1:6379'@'redis://redis:6379'@g" /etc/timesketch.conf
-sudo sed -i "s/NEO4J_HOST = '127.0.0.1'/NEO4J_HOST = 'neo4j'/g" /etc/timesketch.conf
+  # Install Docker
+  sudo apt-get update
+  sudo apt-get install docker-ce -y
+  sudo systemctl enable docker
 
-# Deploy the Skadi solution defined in ./docker-compose.yml
-sudo docker-compose up -d
+  # Install Docker-Compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 
-# Create a template in ES that sets the number of replicas for all indexes to 0
-echo "Waiting for ElasticSearch service to respond to requests"
-until $(curl --output /dev/null --silent --head --fail http://localhost:9200); do
-    printf '.'
-    sleep 5
-done
-echo "Setting the ElasticSearch default number of replicas to 0"
+  # Add skadi to docker usergroup
+  sudo usermod -aG docker $SKADI_USER
+}
 
-curl -XPUT 'localhost:9200/_template/number_of_replicas' \
-    -d '{"template": "*","settings": {"number_of_replicas": 0}}' \
-    -H'Content-Type: application/json'
+cdqr_cylr_setup () {
+  # Clone skadi and it's submodules
+  sudo git clone --recurse-submodules --branch $install_branch https://github.com/orlikoski/Skadi.git /opt/Skadi
+  sudo chown -R $SKADI_USER:$SKADI_USER /opt/Skadi
 
-echo "Waiting for Kibana service to respond to requests"
-until $(curl --output /dev/null --silent --head --fail http://localhost:5601); do
-    printf '.'
-    sleep 5
-done
+  # Copy cdqr script to /usr/local/bin
+  sudo cp /opt/Skadi/scripts/cdqr /usr/local/bin/cdqr
+  sudo chmod +x /usr/local/bin/cdqr
 
-echo "Importing Saved Objects to Kibana and setting default index"
-curl -X POST "http://localhost:5601/api/saved_objects/_bulk_create" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' --data-binary @/opt/Skadi/objects/kibana_6.x_cli_import.json
-curl -X POST "http://localhost:5601/api/kibana/settings/defaultIndex" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d '{"value": "06876cd0-dfc5-11e8-bc06-31e345541948"}'
+  # Create CyLR directory
+  sudo mkdir /opt/CyLR/
+  sudo chmod 777 /opt/CyLR
 
+  # Installs and Configures CDQR and CyLR
+  sudo -E bash /opt/Skadi/scripts/update.sh
+}
+
+timesketch_configs () {
+  # Write TS and Postgres creds to .env file
+  cd /opt/Skadi/Docker/
+  echo TIMESKETCH_USER=$TIMESKETCH_USER > ./.env
+  echo TIMESKETCH_PASSWORD=$TIMESKETCH_PASSWORD >> ./.env
+  echo POSTGRES_USER=$POSTGRES_USER >> ./.env
+  echo POSTGRES_PASSWORD=$psql_pw >> ./.env
+  echo HEAP_SIZE=1g >> ./.env
+
+  # Configure /etc/hosts file so the host can use same names for each service as the TimeSketch Dockers
+  echo 127.0.0.1       elasticsearch |sudo tee -a /etc/hosts
+  echo 127.0.0.1       postgres |sudo tee -a /etc/hosts
+  echo 127.0.0.1       neo4j |sudo tee -a /etc/hosts
+  echo 127.0.0.1       redis |sudo tee -a /etc/hosts
+
+  # Write TimeSketch config file on host
+  sudo cp /opt/Skadi/Docker/timesketch/timesketch.conf /etc/
+  sudo sed -i "s@SECRET_KEY = '<KEY_GOES_HERE>'@SECRET_KEY = '$SECRET_KEY'@g" /etc/timesketch.conf
+  sudo sed -i "s@<USERNAME>\:<PASSWORD>@$POSTGRES_USER\:$psql_pw@g" /etc/timesketch.conf
+  sudo sed -i "s@NEO4J_USERNAME = 'neo4j'@NEO4J_USERNAME = '$neo4juser'@g" /etc/timesketch.conf
+  sudo sed -i "s@NEO4J_PASSWORD = '<NEO4J_PASSWORD>'@NEO4J_PASSWORD = ''@g" /etc/timesketch.conf
+  sudo sed -i "s/UPLOAD_ENABLED = False/UPLOAD_ENABLED = True/g" /etc/timesketch.conf
+  sudo sed -i "s/GRAPH_BACKEND_ENABLED = False/GRAPH_BACKEND_ENABLED = True/g" /etc/timesketch.conf
+  sudo sed -i "s#@localhost/timesketch#@postgres/timesketch#g" /etc/timesketch.conf
+  sudo sed -i "s/ELASTIC_HOST = '127.0.0.1'/ELASTIC_HOST = 'elasticsearch'/g" /etc/timesketch.conf
+  sudo sed -i "s@'redis://127.0.0.1:6379'@'redis://redis:6379'@g" /etc/timesketch.conf
+  sudo sed -i "s/NEO4J_HOST = '127.0.0.1'/NEO4J_HOST = 'neo4j'/g" /etc/timesketch.conf
+
+  # Setup Nginx Auth
+  echo $NGINX_PASSWORD | sudo htpasswd -i -c /etc/nginx/.skadi_auth $NGINX_USER
+}
+
+clean_APT () {
+  # Clean APT
+  sudo apt-get -y autoremove --purge
+  sudo apt-get -y clean
+  sudo apt-get -y autoclean
+}
+
+containers_up () {
+  # Deploy the Skadi solution defined in ./docker-compose.yml
+  sudo docker-compose up -d
+
+  # Create a template in ES that sets the number of replicas for all indexes to 0
+  echo "Waiting for ElasticSearch service to respond to requests"
+  until $(curl --output /dev/null --silent --head --fail http://localhost:9200); do
+      printf '.'
+      sleep 5
+  done
+  echo "Setting the ElasticSearch default number of replicas to 0"
+
+  curl -XPUT 'localhost:9200/_template/number_of_replicas' \
+      -d '{"template": "*","settings": {"number_of_replicas": 0}}' \
+      -H'Content-Type: application/json'
+
+  echo "Waiting for Kibana service to respond to requests"
+  until $(curl --output /dev/null --silent --head --fail http://localhost:5601); do
+      printf '.'
+      sleep 5
+  done
+
+  echo "Importing Saved Objects to Kibana and setting default index"
+  curl -X POST "http://localhost:5601/api/saved_objects/_bulk_create" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' --data-binary @/opt/Skadi/objects/kibana_6.x_cli_import.json
+  curl -X POST "http://localhost:5601/api/kibana/settings/defaultIndex" -H "Content-Type: application/json" -H "kbn-xsrf: true" -d '{"value": "06876cd0-dfc5-11e8-bc06-31e345541948"}'
+}
+
+ensure_TS_up () {
 # The TimeSketch container needs to be running before continuing and this
 # requires the other containers to be up and running too. This can take time
 # so this loop ensures all the parts are running and timesketch is responding
@@ -232,27 +260,20 @@ until $(curl --output /dev/null --silent --head --fail http://localhost/timesket
     sleep 10
 done
 echo "TimeSketch available. Continuing"
+}
 
-# Install Grafana for Monitoring
-git clone https://github.com/orlikoski/skadi_dockprom.git
-cd skadi_dockprom
+grafana_setup () {
+  # Install Grafana for Monitoring
+  git clone https://github.com/orlikoski/skadi_dockprom.git
+  cd skadi_dockprom
 
-# Write Grafana login creds to .env file
-echo ADMIN_USER=$GRAFANA_USER > ./.env
-echo ADMIN_PASSWORD=$GRAFANA_PASSWORD >> ./.env
+  # Write Grafana login creds to .env file
+  echo ADMIN_USER=$GRAFANA_USER > ./.env
+  echo ADMIN_PASSWORD=$GRAFANA_PASSWORD >> ./.env
 
-# This uses the docker-compose.yml found in the skadi_dockprom repo
-sudo docker-compose up -d
-
-# Installs and Configures CDQR and CyLR
-sudo -E bash /opt/Skadi/scripts/update.sh
-
-# Enable and Configure UFW Firewall
-echo "Enabling UFW firewall to only allow OpenSSH and Ngninx Full"
-sudo ufw allow 80
-sudo ufw allow 443
-sudo ufw allow 'OpenSSH'
-sudo ufw --force enable
+  # This uses the docker-compose.yml found in the skadi_dockprom repo
+  sudo docker-compose up -d
+}
 
 echo ""
 echo ""
