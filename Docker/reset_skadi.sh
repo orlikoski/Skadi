4fd848ed78bafd16f4be5bfe70d9992cb50df70c
@AlanOrlikoski <9388572+orlikoski@users.noreply.github.com>
2019-07-12 20:23:20 -0500

    Migrating to one file

---
+++ Docker/reset_skadi.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+set -e
+
+hello_message () {
+  echo "Completely resetting Skadi and removing all saved data"
+  read -n 1 -r -s -p "Press any key to continue... or CTRL+C to exit (nothing has been deleted yet)"
+  echo ""
+}
+
+reset_docker () {
+  set -x
+  docker-compose down
+  docker volume prune --force
+  docker system prune --force
+}
+
+
+############ MAIN PROGRAM #############
+hello_message
+reset_docker
