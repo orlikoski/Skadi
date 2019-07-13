c7f190a22b0eb5bf094ceb8c92b001d195ea0958
Alan Orlikoski <alan@orlikoski.com>
2019-07-13 15:34:33 -0500

    Making Docker scripts and adding persistence

--- Docker/stop_skadi.ps1
+++ Docker/stop_skadi.ps1
@@ -5,16 +5,14 @@ $ErrorActionPreference = "Stop"
 $banner=$env:BANNER
 
 function hello_message {
-  echo "Completely resetting Skadi and removing all saved data"
-  echo "If you already have this configured press any key to continue... or CTRL+C to exit"
+  echo "Stopping  Skadi"
+    echo "If you want to stop the container, and preserve the data, press any key to continue... or CTRL+C to exit"
   $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
   echo ""
 }
 
-function reset_docker () {
-  docker-compose -f .\docker-compose-windows.yml down
-  docker volume prune --force
-  docker system prune --force
+function stop_docker {
+  docker-compose -f .\docker-compose-windows.yml stop
 }
 
 
@@ -23,4 +21,4 @@ if ( $banner -ne "false" ) {
   hello_message
 }
 
-reset_docker
+stop_docker
