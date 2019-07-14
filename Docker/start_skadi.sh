8a4d546114ff007a944f751d781a21a960a9f4c0
Alan Orlikoski <alan@orlikoski.com>
2019-07-13 19:43:54 -0500

    Making docker version start easier

--- Docker/start_skadi.sh
+++ Docker/start_skadi.sh
@@ -6,7 +6,7 @@ banner=${BANNER:-true}
 
 hello_message () {
   echo "Starting a secure dockerized container setup of Skadi"
-  echo "Please ensure you have at least 8 GB RAM and 4 cores allocated to the host"
+  echo "Please ensure you have at least 8 GB RAM and 4 cores allocated to docker"
   read -n 1 -r -s -p "If you already have this configured press any key to continue... or CTRL+C to exit (nothing has been installed)"
   echo ""
 }
