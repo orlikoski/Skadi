9c929396b2293d96df0d1adf0d6477bf796a4d65
Luke <lkuik@shaw.ca>
2019-07-19 11:30:05 -0500

    removing default timesketch conf file as not required

--- scripts/signedbuildskadi.sh
+++ scripts/signedbuildskadi.sh
@@ -197,6 +197,7 @@ timesketch_configs () {
   echo ""
   echo "Setting up Timesketch configs with custom credentials"
   echo ""
+  sudo rm -rf /opt/Skadi/Docker/timesketch/timesketch.conf
   cp /opt/Skadi/Docker/timesketch/timesketch_default.conf /opt/Skadi/Docker/timesketch/timesketch_default.conf.bak
   # Write TS and Postgres creds to .env file
   sudo sed -i -E "s@TIMESKETCH_USER=.*@TIMESKETCH_USER=$TIMESKETCH_USER@g" /opt/Skadi/Docker/.env
