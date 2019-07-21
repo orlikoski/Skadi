f62b24120079285e4054a6b72b1b74b04893230c
Alan Orlikoski <alan@orlikoski.com>
2019-07-20 19:51:11 -0500

    Updating messaging

--- Docker/start_skadi.sh
+++ Docker/start_skadi.sh
@@ -7,7 +7,7 @@ banner=${BANNER:-true}
 hello_message () {
   echo "Starting a secure dockerized container setup of Skadi"
   echo "Please ensure you have at least 8 GB RAM and 4 cores allocated to docker"
-  echo "Ports 22, 80, 5432, 9200 must be available. If not, it will not work correctly"
+  echo "Ports 80, 5432, 9200 must be available. If not, it will not work correctly"
   read -n 1 -r -s -p "If this is configured press any key to continue... or CTRL+C to exit (nothing has been installed)"
   echo ""
 }
